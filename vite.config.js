69224141dd8e982440a247fe1d32b01642a33026
srya <m.surya.dharma9f@gmail.com>
2024-09-22 18:39:46 +0700

    second

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'cobadulu', // Ganti dengan nama repository GitHub kamu
+  base: '/cobadulu/', // Ganti dengan nama repository GitHub kamu
 });
