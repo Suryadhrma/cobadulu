2276d53ca7cc676fbb67618f446f09d4c550e6da
srya <m.surya.dharma9f@gmail.com>
2024-09-22 18:57:40 +0700

    baru lagi

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/cobadulu/', // Ganti dengan nama repository GitHub kamu
-});
+  build: {
+    sourcemap: true, // Aktifkan sourcemap jika diperlukan
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Semua dependensi dari node_modules akan dimasukkan ke dalam chunk vendor
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Naikkan batas peringatan ukuran chunk
+  },
+}
+);
